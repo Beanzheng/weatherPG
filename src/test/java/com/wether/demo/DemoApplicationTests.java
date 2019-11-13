917df078db3cf69062723aecb7daf6306fdbeb52
beanzheng <Bean_zheng@163.com>
2019-11-13 12:02:15 +0800

    1.搭建开发环境，导入配置文件 2.新建天气预报系统说明设计文档

---
+++ src/test/java/com/wether/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wether.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
